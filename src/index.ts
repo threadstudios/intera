b83a7d0308caa9ab2cf6b69d09216681c0377fc0
Paul Westerdale <paul@westerdale.me>
2025-05-25 18:33:08 +0100

    feat: add vault types to extend request

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,7 @@ import fastifyCookie from "@fastify/cookie";
 import fastifyCors from "@fastify/cors";
 import fastifyExpress from "@fastify/express";
 import fastifyStatic from "@fastify/static";
-import Fastify, {
-	FastifyReply,
-	FastifyRequest,
-	type FastifyInstance,
-} from "fastify";
+import Fastify, { type FastifyInstance } from "fastify";
 import { match } from "ts-pattern";
 import { Container } from "typedi";
 import { Intera__RouterCache } from "./caches/RouterCache";
@@ -23,150 +19,146 @@ import { Logger } from "./logger";
 import { buildMiddlewares } from "./route/buildMiddlewares";
 import { validate } from "./route/validate";
 import type { InteraConfig } from "./types/config.types";
-import type {
-	InteraMiddleware,
-	InteraReply,
-	InteraRequest,
-} from "./types/router.types";
+import type { InteraReply, InteraRequest } from "./types/router.types";
 
 export async function InteraServer({
-	coreModule,
-	config,
+  coreModule,
+  config,
 }: {
-	coreModule: Function;
-	config: InteraConfig;
+  coreModule: Function;
+  config: InteraConfig;
 }) {
-	const configClass = new Intera__Config(
-		config.apiClientPath,
-		config.apiBaseUrl,
-	);
-	Container.set(Intera__Config, configClass);
-	Container.get(coreModule);
-	const loggerInstance = new Logger();
-	Container.set(Logger, loggerInstance);
+  const configClass = new Intera__Config(
+    config.apiClientPath,
+    config.apiBaseUrl
+  );
+  Container.set(Intera__Config, configClass);
+  Container.get(coreModule);
+  const loggerInstance = new Logger();
+  Container.set(Logger, loggerInstance);
 
-	const fastify: FastifyInstance = Fastify();
+  const fastify: FastifyInstance = Fastify();
 
-	await fastify.register(fastifyCookie);
-	await fastify.register(fastifyExpress);
-	await fastify.register(fastifyCors);
-	fastify.register(fastifyStatic, {
-		root: `${process.cwd()}/public`,
-		prefix: "/public/",
-	});
+  await fastify.register(fastifyCookie);
+  await fastify.register(fastifyExpress);
+  await fastify.register(fastifyCors);
+  fastify.register(fastifyStatic, {
+    root: `${process.cwd()}/public`,
+    prefix: "/public/",
+  });
 
-	const routes = Container.get(Intera__RouterCache).getAllRoutes();
-	const finalCodegenRoutes = routes.map((routeRecord) => {
-		const controllerRoute = getControllerRoute(routeRecord.target.constructor);
+  const routes = Container.get(Intera__RouterCache).getAllRoutes();
+  const finalCodegenRoutes = routes.map((routeRecord) => {
+    const controllerRoute = getControllerRoute(routeRecord.target.constructor);
 
-		const routePath = match(routeRecord)
-			.with({ method: "*" }, () => {
-				return routeRecord.route;
-			})
-			.otherwise(() => {
-				return controllerRoute
-					? `${controllerRoute}${routeRecord.route}`
-					: routeRecord.route;
-			});
+    const routePath = match(routeRecord)
+      .with({ method: "*" }, () => {
+        return routeRecord.route;
+      })
+      .otherwise(() => {
+        return controllerRoute
+          ? `${controllerRoute}${routeRecord.route}`
+          : routeRecord.route;
+      });
 
-		return {
-			...routeRecord,
-			route: routePath,
-		};
-	});
+    return {
+      ...routeRecord,
+      route: routePath,
+    };
+  });
 
-	withOpenApiGenerator({
-		required: config.withOpenApi ?? false,
-		routes: finalCodegenRoutes,
-	});
+  withOpenApiGenerator({
+    required: config.withOpenApi ?? false,
+    routes: finalCodegenRoutes,
+  });
 
-	await withScalar({
-		required: config.withScalar ?? false,
-		fastify,
-	});
+  await withScalar({
+    required: config.withScalar ?? false,
+    fastify,
+  });
 
-	for (const routeRecord of finalCodegenRoutes) {
-		const { method, target, handler, route, middlewares, schemas } =
-			routeRecord;
-		const controllerInstance = Container.get(target.constructor);
-		if (method === "*") {
-			fastify.use(
-				route,
-				(req: InteraRequest, res: InteraReply, done: (err?: Error) => void) => {
-					const builtMiddleware = buildMiddlewares(middlewares);
-					Promise.all(builtMiddleware.map((bm) => bm(req, res)))
-						.then(() => done())
-						.catch((err) => {
-							done(err);
-						});
-				},
-			);
-		} else {
-			const isDeleteOrGet = ["delete", "get"].includes(method);
-			loggerInstance.info(
-				`Registering route ${method.toUpperCase()}\t${route}`,
-			);
-			fastify[method](
-				route,
-				{
-					preHandler: buildMiddlewares(middlewares),
-				},
-				async (request, reply) => {
-					try {
-						if (schemas?.[0]) {
-							validate(
-								schemas[0],
-								isDeleteOrGet ? request.query : request.body,
-								"Request Input",
-							);
-						}
-						const args = Array.from(
-							{ length: handler.length },
-							() => undefined,
-						);
-						extractAllParams(
-							target,
-							handler.name,
-							args,
-							request as InteraRequest,
-							reply,
-						);
-						const result = await handler.apply(controllerInstance, args);
-						if (schemas?.[1]) {
-							validate(schemas[1], result, "Request Output");
-						}
-						reply.send(result);
-					} catch (error: unknown) {
-						if (error instanceof HttpError) {
-							reply
-								.code(error.statusCode)
-								.send({ error: error.message, ...error.body });
-							return;
-						}
-						loggerInstance.error("Uncaught Service Error", {
-							message: (error as Error).message,
-						});
-						reply.code(500).send({ error: (error as Error).message });
-					}
-				},
-			);
-		}
-	}
+  for (const routeRecord of finalCodegenRoutes) {
+    const { method, target, handler, route, middlewares, schemas } =
+      routeRecord;
+    const controllerInstance = Container.get(target.constructor);
+    if (method === "*") {
+      fastify.use(
+        route,
+        (req: InteraRequest, res: InteraReply, done: (err?: Error) => void) => {
+          const builtMiddleware = buildMiddlewares(middlewares);
+          Promise.all(builtMiddleware.map((bm) => bm(req, res)))
+            .then(() => done())
+            .catch((err) => {
+              done(err);
+            });
+        }
+      );
+    } else {
+      const isDeleteOrGet = ["delete", "get"].includes(method);
+      loggerInstance.info(
+        `Registering route ${method.toUpperCase()}\t${route}`
+      );
+      fastify[method](
+        route,
+        {
+          preHandler: buildMiddlewares(middlewares),
+        },
+        async (request, reply) => {
+          try {
+            if (schemas?.[0]) {
+              validate(
+                schemas[0],
+                isDeleteOrGet ? request.query : request.body,
+                "Request Input"
+              );
+            }
+            const args = Array.from(
+              { length: handler.length },
+              () => undefined
+            );
+            extractAllParams(
+              target,
+              handler.name,
+              args,
+              request as InteraRequest,
+              reply
+            );
+            const result = await handler.apply(controllerInstance, args);
+            if (schemas?.[1]) {
+              validate(schemas[1], result, "Request Output");
+            }
+            reply.send(result);
+          } catch (error: unknown) {
+            if (error instanceof HttpError) {
+              reply
+                .code(error.statusCode)
+                .send({ error: error.message, ...error.body });
+              return;
+            }
+            loggerInstance.error("Uncaught Service Error", {
+              message: (error as Error).message,
+            });
+            reply.code(500).send({ error: (error as Error).message });
+          }
+        }
+      );
+    }
+  }
 
-	withApiClient({
-		required: config.withClientGenerator,
-	});
+  withApiClient({
+    required: config.withClientGenerator,
+  });
 
-	await fastify.ready();
+  await fastify.ready();
 
-	return fastify;
+  return fastify;
 }
 
 export { Service } from "typedi";
 export { Module } from "./decorators/Module";
 export { Controller } from "./decorators/Controller";
 export type {
-	InteraRequest,
-	InteraReply,
-	InteraMiddleware,
+  InteraRequest,
+  InteraReply,
+  InteraMiddleware,
 } from "./types/router.types";
--- src/types/router.types.ts
+++ src/types/router.types.ts
@@ -3,51 +3,53 @@ import { Token } from "typedi";
 import { ZodAny } from "zod/v4";
 
 export type RouterCacheRecord = {
-	route: string;
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-	handler?: any;
-	target: object;
-	parent?: object;
-	method:
-		| "get"
-		| "post"
-		| "put"
-		| "delete"
-		| "patch"
-		| "options"
-		| "head"
-		| "*";
-	middlewares?: Token<unknown>[];
-	input?: unknown;
-	output?: unknown;
-	schemas?: [ZodAny, ZodAny];
+  route: string;
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  handler?: any;
+  target: object;
+  parent?: object;
+  method:
+    | "get"
+    | "post"
+    | "put"
+    | "delete"
+    | "patch"
+    | "options"
+    | "head"
+    | "*";
+  middlewares?: Token<unknown>[];
+  input?: unknown;
+  output?: unknown;
+  schemas?: [ZodAny, ZodAny];
 };
 
 export type RouterCacheRecordPatchable = {
-	input: unknown;
-	ouput: unknown;
+  input: unknown;
+  ouput: unknown;
 };
 
 export enum RestMethods {
-	Get = "get",
-	Post = "post",
-	Put = "put",
-	Delete = "delete",
-	Patch = "patch",
-	Options = "options",
-	Head = "head",
-	All = "*",
+  Get = "get",
+  Post = "post",
+  Put = "put",
+  Delete = "delete",
+  Patch = "patch",
+  Options = "options",
+  Head = "head",
+  All = "*",
 }
 
 export interface InteraMiddleware {
-	run: (request: InteraRequest, reply: InteraReply) => Promise<void>;
+  run: (request: InteraRequest, reply: InteraReply) => Promise<void>;
 }
 
-export interface InteraRequest extends FastifyRequest {
-	params: Record<string, string>;
-	body: Record<string, unknown>;
-	query: Record<string, string>;
-	cookies: Record<string, string>;
+export interface InteraRequest<T extends Record<string, unknown> = {}>
+  extends FastifyRequest {
+  params: Record<string, string>;
+  body: Record<string, unknown>;
+  query: Record<string, string>;
+  cookies: Record<string, string>;
+  vault: T;
 }
 
 export interface InteraReply extends FastifyReply {}
