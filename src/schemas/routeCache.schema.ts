3523879170fffca2cf6714ac340c5f8bd6fa8749
Paul Westerdale <paul@westerdale.me>
2025-07-29 14:04:19 +0100

    hotfix: validation of routes

--- src/schemas/routeCache.schema.ts
+++ src/schemas/routeCache.schema.ts
@@ -1,16 +1,16 @@
 import z from "zod/v4";
 
 export const routeCacheItemSchema = z.object({
-	route: z.string(),
-	target: z.object(),
-	method: z.enum([
-		"get",
-		"post",
-		"put",
-		"delete",
-		"patch",
-		"options",
-		"head",
-		"*",
-	]),
+  route: z.string().optional(),
+  target: z.object(),
+  method: z.enum([
+    "get",
+    "post",
+    "put",
+    "delete",
+    "patch",
+    "options",
+    "head",
+    "*",
+  ]),
 });
